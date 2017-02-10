23315c1f7e514dc18430193a280e63322f1fa2c4
Zeno Popovici <zeno@graffino.com>
2017-02-10 21:53:40 +0200

    Restructure

---
+++ gulpfile.js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Gulp config file
+ * Author: Graffino (http://www.graffino.com)
+ */
+
+/*
+  index.js
+  ===========
+  Gulp tasks live in the `gulpfile.js/tasks` folder and get automatically
+  required below.
+
+  gulpfile.js/tasks/default.js is run when you calling `gulp`.
+*/
+
+var requireDir = require('require-dir');
+
+// Require all tasks in gulpfile.js/tasks, including subfolders
+requireDir('./tasks', {recurse: true});
