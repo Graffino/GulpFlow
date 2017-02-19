895c597fd42cbac35813520545d2be236c8353c5
Zeno Popovici <zeno@graffino.com>
2017-02-19 16:59:57 +0200

    Bugfixes

--- gulpfile.js/webpack.config.js
+++ gulpfile.js/webpack.config.js
@@ -36,7 +36,10 @@ var webpackDevelopment = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015']
+          presets: [
+            'es2015',
+            'stage-0'
+          ]
         }
       },
       {
