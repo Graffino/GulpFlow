30af01d3097c7c83be91e4759a9f6b324943171f
Zeno Popovici <zeno@graffino.com>
2017-06-21 20:21:53 +0300

    Update defaults

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -53,7 +53,7 @@ const enabled = {
   nunjucks: false,
 
   // PHP Composer
-  composer: true,
+  composer: false,
 
   // Wordpress
   wordpress: {
@@ -72,7 +72,7 @@ const enabled = {
     css: true,
     js: true,
     html: true,
-    php: true
+    php: false
   },
 
   // Watch (Only for PHP linting)
@@ -105,7 +105,7 @@ const clean = {
     images: true,
     icons: true,
     vendor: true,
-    wordpress: true
+    wordpress: false
   }
 };
 
